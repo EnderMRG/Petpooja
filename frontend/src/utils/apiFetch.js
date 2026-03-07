c925fbd852492efe1dd0f1cc835cbdc45fb0376d
EnderMRG <moharnab.gogoi@gmail.com>
2026-03-07 14:44:27 +0530

    few fixes

--- frontend/src/utils/apiFetch.js
+++ frontend/src/utils/apiFetch.js
@@ -5,7 +5,7 @@
  * Usage:  import apiFetch from '../utils/apiFetch'
  *         const res = await apiFetch('/orders/active')
  */
-export default function apiFetch(endpoint, options = {}) {
+export default function apiFetch(url, options = {}) {
     let restaurantId = null
     try {
         const user = JSON.parse(sessionStorage.getItem('pp_user') || 'null')
@@ -16,10 +16,6 @@ export default function apiFetch(endpoint, options = {}) {
         ...(options.headers || {}),
         ...(restaurantId ? { 'X-Restaurant-ID': restaurantId } : {}),
     }
-
-    // Use environment variable if deployed, otherwise fallback to relative (which uses vite proxy)
-    const baseUrl = import.meta.env.VITE_API_BASE_URL || ''
-    const url = `${baseUrl}${endpoint}`
 
     return fetch(url, { ...options, headers })
 }
